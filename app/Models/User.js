7bebdf3290a3bec4767ad49e3fd1fbfd29650cfe
tandavala <jose.tandavala@gmail.coom>
2019-07-18 21:50:06 +0100

    criando rea criando relacao entre usuario e trabalho

--- app/Models/User.js
+++ app/Models/User.js
@@ -21,6 +21,9 @@ class User extends Model {
     })
   }
 
+  jobs() {
+    return this.hasMany('App/Models/Jobs')
+  }
   /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
