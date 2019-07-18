8d98cd7a815ab2b82a976c9da3b4d3ebc9a60dbf
tandavala <jose.tandavala@gmail.coom>
2019-07-18 19:19:19 +0100

    coding the validator

--- app/Validators/CreateUser.js
+++ app/Validators/CreateUser.js
@@ -3,8 +3,22 @@
 class CreateUser {
   get rules () {
     return {
-      // validation rules
+      'username' : 'required|unique:users',
+      'email' : 'required|unique:users',
+      'password' : 'required'
     }
+  }
+  get messages() {
+    return {
+      'require' : 'Woah now, {{ field }} is required.',
+      'unique' : '{{ field }} already taken.',
+    }
+  }
+  async fails(error){
+    this.ctx.session.withErrors(error)
+    .flashAll();
+
+    return this.ctx.response.redirect('back');
   }
 }
 
