c52fed1f21b3b1851d8fc6437b7978e035174617
tandavala <jose.tandavala@gmail.coom>
2019-07-18 19:50:58 +0100

    dando a possibilidade no sistema de fazer login

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,29 @@
 'use strict'
 
+
+const User = use('App/Models/User')
+
 class UserController {
+    async create({ request, response, auth }){
+        
+         const dataUser = request.only(['name', 'username', 'email', 'password'])
+
+         try {
+             // save user to database
+            const user = await User.create(dataUser)
+            // generate JWT token for user
+            //const token = await auth.generate(user)
+
+            await auth.login(user);
+            return response.redirect('/');
+            console.log('User created')
+
+         } catch (error) {
+             console.log(error)
+         }
+
+         
+    }
 }
 
 module.exports = UserController
